25fa5ce53a0f9341827ed857040575bc1095355f
brapastor <brapastor@gmail.com>
2020-09-23 02:40:42 -0500

    giftapp

---
+++ docs/precache-manifest.8f925dd5a92e7c046c1d0c06ebd66e7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7ea3572511957d009394554804226a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2536c96f24c1f8e6a56b",
+    "url": "/static/css/main.fa1cc47b.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2536c96f24c1f8e6a56b",
+    "url": "/static/js/main.84814a23.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
